d8034b43e32a6a1aaaeed37342c51754397782f3
Samuel Lampa <samuel.lampa@gmail.com>
2015-06-11 19:43:11 +0200

    Starting to loop over image files

--- convert.pas
+++ convert.pas
@@ -20,13 +20,29 @@ var
   i: integer;
   plateDirObj: TString;
   plateDirPath: String;
+  imgFileNameExpr: TRegExpr;
+  imgFilePaths: TStringList;
+  imgFilePath: String;
+
 begin
+  imgFileNameExpr := TRegExpr.Create;
+  imgFileNameExpr.Expression := '([\w\d\s]+)_([\w\d]+)_(s\d)_(w\d)(_thumb)?.*';
+
   for i := 0 to plateDirs.Count-1 do
   begin
     plateDirName := plateDirs.Strings[i];
     plateDirObj := TString(plateDirs.Objects[i]);
     plateDirPath := plateDirObj.Text;
     ShowMessage('Plate dir: ' + plateDirName + LineEnding + 'With directory:' + LineEnding + plateDirPath);
+    // Loop over image files in plate directory
+    // TODO: Remember to check for possible "timepoint" folders here
+    imgFilePaths := TStringList.Create;
+    imgFilePaths := FileUtil.FindAllFiles(plateDirPath);
+    for imgFilePath in imgFilePaths do
+    begin
+      ShowMessage('Image file: ' + LineEnding + imgFilePath);
+    end;
+    imgFilePaths.Free;
   end;
 
   //for experimentDir in experimentDirs do
